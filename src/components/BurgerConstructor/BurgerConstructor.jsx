30c5ad9ba8425cb78b0b2fe25cd7cd13d929c786
zubastu <79757684+zubastu@users.noreply.github.com>
2022-10-01 10:28:47 +0300

    Исправления: Переместил скролл у конструктора бургера

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -15,50 +15,52 @@ const BurgerConstructor = ({
   selectedIngredients,
 }) => {
   return (
-    <section className={`${burgerConstructorStyles.container} custom-scroll`}>
+    <section className={`${burgerConstructorStyles.container} `}>
       <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
       <MyTab />
-      <h2 className="text text_type_main-medium mt-10">Булки</h2>
-      <div
-        className={`${burgerConstructorStyles.material_container} pt-6 pl-4 pr-4`}
-      >
-        {bun &&
-          bun.map((bunItem) => (
-            <MaterialItem
-              selectedBun={selectedBun}
-              material={bunItem}
-              key={bunItem._id}
-              onSelect={selectBun}
-            />
-          ))}
-      </div>
-      <h2 className="text text_type_main-medium mt-10">Соусы</h2>
-      <div
-        className={`${burgerConstructorStyles.material_container} pt-6 pl-4 pr-4`}
-      >
-        {sauces &&
-          sauces.map((sauce) => (
-            <MaterialItem
-              selectedIngredients={selectedIngredients}
-              material={sauce}
-              key={sauce._id}
-              onSelect={selectIngredient}
-            />
-          ))}
-      </div>
-      <h2 className="text text_type_main-medium mt-10">Начинки</h2>
-      <div
-        className={`${burgerConstructorStyles.material_container} pt-6 pl-4 pr-4`}
-      >
-        {main &&
-          main.map((mainItem) => (
-            <MaterialItem
-              selectedIngredients={selectedIngredients}
-              material={mainItem}
-              key={mainItem._id}
-              onSelect={selectIngredient}
-            />
-          ))}
+      <div className={`${burgerConstructorStyles.scroll} custom-scroll mt-10`}>
+        <h2 className="text text_type_main-medium">Булки</h2>
+        <div
+          className={`${burgerConstructorStyles.material_container} pt-6 pl-4 pr-4`}
+        >
+          {bun &&
+            bun.map((bunItem) => (
+              <MaterialItem
+                selectedBun={selectedBun}
+                material={bunItem}
+                key={bunItem._id}
+                onSelect={selectBun}
+              />
+            ))}
+        </div>
+        <h2 className="text text_type_main-medium mt-10">Соусы</h2>
+        <div
+          className={`${burgerConstructorStyles.material_container} pt-6 pl-4 pr-4`}
+        >
+          {sauces &&
+            sauces.map((sauce) => (
+              <MaterialItem
+                selectedIngredients={selectedIngredients}
+                material={sauce}
+                key={sauce._id}
+                onSelect={selectIngredient}
+              />
+            ))}
+        </div>
+        <h2 className="text text_type_main-medium mt-10">Начинки</h2>
+        <div
+          className={`${burgerConstructorStyles.material_container} pt-6 pl-4 pr-4`}
+        >
+          {main &&
+            main.map((mainItem) => (
+              <MaterialItem
+                selectedIngredients={selectedIngredients}
+                material={mainItem}
+                key={mainItem._id}
+                onSelect={selectIngredient}
+              />
+            ))}
+        </div>
       </div>
     </section>
   );
