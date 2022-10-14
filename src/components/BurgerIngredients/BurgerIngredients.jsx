bf55d8392466a898bdf11adf86487d3dde1930c4
zubastu <79757684+zubastu@users.noreply.github.com>
2022-10-14 17:59:12 +0300

    Complete: Добавил индикацию табов на протяжении скролла контейнера

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import burgerConstructorStyles from "./BurgerIngredients.module.css";
-import MyTab from "../MyTab/MyTab";
 import MaterialItem from "../MaterialItem/MaterialItem";
 import PropTypes from "prop-types";
 import { INGREDIENT_TYPES } from "../../utils/constants";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import { useObserver } from "../../hoocs/useObserver";
 
 const BurgerIngredients = ({
   bun,
@@ -14,15 +15,56 @@ const BurgerIngredients = ({
   selectedIngredients,
   selectBun = null,
 }) => {
+  const [current, setCurrent] = useState("one");
+
+  const sauceRef = useRef();
+  const bunRef = useRef();
+  const mainRef = useRef();
+
+  let observerBun = useObserver(bunRef);
+  let observerSauces = useObserver(sauceRef);
+  let observerMain = useObserver(mainRef);
+
+  console.log(observerMain);
+
+  useEffect(() => {
+    const element = document.getElementById(current);
+    element.scrollIntoView({ behavior: "smooth" });
+  }, [current]);
+
   return (
-    <section className={`${burgerConstructorStyles.container} `}>
+    <section
+      id="ingredients-container"
+      className={`${burgerConstructorStyles.container} `}
+    >
       <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
-      <MyTab />
+
+      <div style={{ display: "flex" }}>
+        <Tab value="one" active={observerBun.intersecting} onClick={setCurrent}>
+          Булки
+        </Tab>
+        <Tab
+          value="two"
+          active={observerSauces.intersecting}
+          onClick={setCurrent}
+        >
+          Соусы
+        </Tab>
+        <Tab
+          value="three"
+          active={observerMain.intersecting}
+          onClick={setCurrent}
+        >
+          Начинки
+        </Tab>
+      </div>
+
       <div className={`${burgerConstructorStyles.scroll} custom-scroll mt-10`}>
         <h2 id="one" className="text text_type_main-medium">
           Булки
         </h2>
         <div
+          ref={bunRef}
           className={`${burgerConstructorStyles.material_container} pt-6 pl-4 pr-4`}
         >
           {bun &&
@@ -39,6 +81,7 @@ const BurgerIngredients = ({
           Соусы
         </h2>
         <div
+          ref={sauceRef}
           className={`${burgerConstructorStyles.material_container} pt-6 pl-4 pr-4`}
         >
           {sauces &&
@@ -55,6 +98,7 @@ const BurgerIngredients = ({
           Начинки
         </h2>
         <div
+          ref={mainRef}
           className={`${burgerConstructorStyles.material_container} pt-6 pl-4 pr-4`}
         >
           {main &&
