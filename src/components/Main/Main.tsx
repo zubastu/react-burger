2ed840b37b5643d04de342d1c2c5864897a1d43b
zubastu <79757684+zubastu@users.noreply.github.com>
2023-04-20 20:51:22 +0300

    Добавил красивую превью для перетаскиваемых элементов

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,18 +1,40 @@
-import React, { FC } from "react";
+import React, { CSSProperties, FC } from "react";
 import styles from "./Main.module.css";
 import { DndProvider } from "react-dnd";
-import { MultiBackend } from "react-dnd-multi-backend";
-import { HTML5toTouch } from "rdndmb-html5-to-touch";
-import { TChildrenNode } from "../../types";
+import { TouchBackend } from "react-dnd-touch-backend";
+import { usePreview } from "react-dnd-preview";
+
+import { TChildrenNode, TConstructorIngredient } from "../../types";
 
 type TMain = {
   children: TChildrenNode;
 };
+type TPreview = {
+  display: boolean;
+  itemType: string;
+  item: TConstructorIngredient;
+  style: CSSProperties;
+};
 
 const Main: FC<TMain> = ({ children }) => {
+  const MyPreview = () => {
+    const { display, item, style } = usePreview() as TPreview;
+    if (!display) {
+      return null;
+    }
+    return (
+      <img
+        className={styles.image}
+        src={item.image}
+        style={style}
+        alt="image"
+      />
+    );
+  };
   return (
-    <DndProvider backend={MultiBackend} options={HTML5toTouch}>
+    <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
       <main className={styles.main}>{children}</main>
+      <MyPreview />
     </DndProvider>
   );
 };
